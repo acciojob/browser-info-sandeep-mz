848c7f4d4fa3157062ae7bae91cebaaf9c5d84ab
Sandeep Maurya <108665091+sandeep-mz@users.noreply.github.com>
2023-09-22 18:43:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-//your JS code here. If required.
-
 // Get browser information
-const userAgent = navigator.userAgent;
 const browserName = navigator.appName;
 const browserVersion = navigator.appVersion;
 
-// Display browser information in the specified format
-const browserInfo = `You are using ${browserName} version ${browserVersion}`;
-document.getElementById().textContent = browserInfo;
+// Create message
+const message = `You are using ${browserName} version ${browserVersion}`;
+
+// Display message
+document.getElementById('browser-info').textContent = message;
